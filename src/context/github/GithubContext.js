9b5bb928032bc1cc62e2460d203b21dcfe46ae13
krating20 <56456483+krating20@users.noreply.github.com>
2024-03-10 14:39:30 +0700

    actions

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,52 +1,28 @@
-import { createContext, useReducer } from "react";
-import githubReducer from "./GithubReducer";
-const GithubContext = createContext();
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
 
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+const GithubContext = createContext()
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
-  };
+  }
 
-  const [state, dispatch] = useReducer(githubReducer, initialState);
+  const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  const searchUsers = async (text) => {
-    setLoading()
-
-    const params = new URLSearchParams({
-        q: text,
-      })
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    });
-
-    const {items} = await response.json()
-    dispatch({
-      type: "GET_USERS",
-      payload: items,
-    });
-  };
-
-const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
-  const setLoading = () => dispatch({type:'SET_LOADING'})
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
+        ...state,
+        dispatch,
       }}
     >
       {children}
     </GithubContext.Provider>
-  );
-};
+  )
+}
 
-export default GithubContext;
+export default GithubContext
